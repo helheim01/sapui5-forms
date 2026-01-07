2fa29fd777746b183ffb380e1b27268b0818d3c5
Jeronimo Debiazi <jeronimo.debiazi@agprodservicios.com>
2026-01-07 19:21:36 +0000

    inicio controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,26 @@
+// @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     */
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("com.devjero.forms.forms.controller.App", {
         onInit: function () {
-            console.log("Controller inicializado");
+            
+            /* Instancio el modelo */
+            const oModel=new JSONModel();
+            oModel.loadData("./localService/mockdata/CustomerModel.json");
+
+            /* Instancio la vista */
+            this.oView=this.getView();
+            this.oView.setModel(oModel);
+
         }
     });
 });
